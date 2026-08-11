53f063a52063b4120d6e7ded24bd60dbf3b977c5
나유성 <yusung052077@gmail.com>
2026-08-11 10:49:41 +0900

    chore: DS_Store 파일 제외

--- inspire_java_grammar_6th/src/VariableApp.java
+++ inspire_java_grammar_6th/src/VariableApp.java
@@ -1,5 +1,20 @@
 public class VariableApp {
+
+    /*
+    변수? 데이터를 담는 그릇
+    - 기본타입(값을 담는 그릇) <- literal value
+        - 숫자형(정수 - byte, short, int, long, 실수 - float, double)
+        - 문자형(char)
+        - 논리형(bolean)
+        - 문자열(String)
+    - 참조타입(주소값을 담는 그릇)
+        - 기본타입이 아닌 모든 것
+    
+    선언문법
+    - 접근지정자(public, private) 변수타입 변수명 = literal value; -> 문장 마지막에 ;은 강제이다.
+    ex) public int age = 10;
+    */
     public static void main(String[] args) {
-        
+        System.out.println();
     }
 }
